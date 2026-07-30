a906c15096b9844ed4d9df5ec2451e3ea2317de2
Ali Mirzaei <ali.mirzaei.kt@gmail.com>
2026-07-30 18:46:36 +0330

    Configure GitHub GraphQL connection

--- scripts/generate_contributions.py
+++ scripts/generate_contributions.py
@@ -4,3 +4,8 @@ import os
 import requests
 USERNAME = "ali-mirzaei-dev"
 TOKEN = os.getenv("GH_TOKEN")
+API = "https://api.github.com/graphql"
+
+headers = {
+    "Authorization": f"Bearer {TOKEN}"
+}
